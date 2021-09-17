f2513596fbd257becaa49acbf9df01cf7250f031
Bri <brianna.velarde16@gmail.com>
2021-09-16 19:58:01 -0500

    next biggest number code

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -2,15 +2,32 @@
 import sys
 
 def main():
-    next_biggest_number(sys.argv[1])
+    next_biggest_number(int(sys.argv[1]))
 
 
 def next_biggest_number(num):
-    #TODO: Implement me!
-    return 0
+  
+    digits = [int(x) for x in str(num)]
+    for i in range(len(digits) - 1, 0, -1):
+    	if digits[i] > digits[i - 1]:
+    		digits2 = digits[i - 1:]
+    		remainder = digits[:i - 1]
+    		trigger = int(digits[i - 1])
+    		digits2.sort()
+    		greater = [i for i in digits2 if i > trigger]
+    		rightone = greater[0]
+    		digits2.remove(rightone)
+    		remainder.append(rightone)
+    		almostoutput = remainder + digits2
+    		strings = [str(integer) for integer in almostoutput]
+    		strings2 = "".join(strings)
+    		output = int(strings2)
+
+
+    		return output
+    	
+
+    return -1
 
 if __name__ == "__main__":
     main()
-
-
-
